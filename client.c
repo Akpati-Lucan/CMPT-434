33237618ef15c779f54dcb9955511d1e48f40e80
Lucan Akpati <jbs671@usask.ca>
2026-01-19 10:38:43 -0600

    added header files for client.c

--- client.c
+++ client.c
@@ -4,6 +4,16 @@
 # Student Number: 11331253
 */
 
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+
+
 int main(){
 
     return 0;
